855534e72c6e8162dceff53d10d17f69231ecd4e
pbpeam <32643186+pbpeam@users.noreply.github.com>
2018-05-27 23:27:14 +0700

    Something

--- CousePage.php
+++ CousePage.php
@@ -7,11 +7,31 @@
     <iframe id="ytplayer" type="text/html" width="100%" height="auto"
   src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
   frameborder="0"></iframe>
-        <div style="margin: 10px ">
-            <p>Course Title</p>
-            <div style="margin: 25px 50px 50px 100px">describe: test.</div>
+        <div style="margin: 30px 30px">
+            <h3>Course Title</h3>
+            <div class="indent">describe: bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~</div>
+            <br>
+            <div class="indent"><b>by:_______</b></div>
+
+            <p><b>Workshop-Schedule</b></p>
+            (workshop-link)
+
+            <h3>Lession</h3>
+            <h5>1.)</h5>
+            <h5>2.)</h5>
+            <h5>3.)</h5>
+            <h5>4.)</h5>
+            <h5>5.)</h5>
+            <h5>5.)</h5>
+            <h5>5.)</h5>
+            <h5>5.)</h5>
+            <h5>5.)</h5>
+            <h5>5.)</h5>
+            <h5>5.)</h5>
+            <h5>5.)</h5>
+
             <br>
         </div>
-    </div>
-</body>
+    </body>
+</div>
 </html>
