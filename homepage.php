6f3bb5d92acefdefb33af030a86b1ce4865b90d9
f r e s h <fresh.thit@me.com>
2018-05-28 00:07:26 +0700

    test query ui

--- homepage.php
+++ homepage.php
@@ -3,9 +3,29 @@
   if ($_SESSION["email"] == null) {
     header("location:login.php");
   }
-  else {
-      echo $_SESSION["email"];
-  }
+  // else {
+  //     echo $_SESSION["email"];
+  // }
+
+  $connection = mysqli_connect("localhost","root","1212312121");
+    mysqli_select_db($connection, "LearnSomething");
+
+  $q = "SELECT name, id FROM instructors"; 
+  $r = mysqli_query($dbc, $q); 
+
+  // while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+  //   $name   = $row['name'];
+  //   $address = $row['address'];
+  //   $content = $row['content'];
+
+  //   //Create new output
+  //   $output  = "<p>$name</p>";
+  //   $output .= "<p>$address</p>";
+  //   $output .= "<p>$content</p>";
+
+  //   //Add output to array
+  //   $mycontent[] = $output;     
+  // }
   // session_write_close();
 ?>
 
@@ -17,14 +37,30 @@
 
     <!-- CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/login_style.css">
+    <!-- <link rel="stylesheet" href="css/login_style.css"> -->
 
     <!-- FONT -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit|Karla">
 
     <title>Login</title>
   </head>
-  <body>
+  <body style="background-color:#F4F1ED">
+    <nav class="navbar navbar-inverse fixed-top" style="width: 100%; height:70px; background: #191917">
+      <a class="navbar-brand" href="#">
+          <img src="img/weblogo.png" alt="logo" style="Height:30px;">
+      </a>
+    </nav>
+
+    <?php
+        echo "<table>";
+        while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+          $name   = $row['name'];
+          $address = $row['id'];
+          // $content = $row['content'];
+          echo "<tr><td>".$name."</td><td>".$address."</td><td>".$content."</td></tr>";
+        }
+        echo "</table>";
+    ?>
     Welcome, you're already logged in :)
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
