7c6743a8ba4a813da4a4f4dd75220e604c8c3cd1
pbpeam <32643186+pbpeam@users.noreply.github.com>
2018-05-27 12:12:45 +0700

    eiei

---
+++ register.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<style>
+    /* <link rel="stylesheet" href="> */
+</style>
+    <link rel="stylesheet" href="register_style.css">
+</head>
+<body>
+    <p class"size50px">Register</p>
+</body>
+</html>
