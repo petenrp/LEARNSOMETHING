43e3eb3c97a52163091647fa278524a099336868
f r e s h <fresh.thit@me.com>
2018-05-27 15:29:10 +0700

    [perform_login.php] add more test for debugging

--- perform_login.php
+++ perform_login.php
@@ -1,9 +1,31 @@
 <?php
     // DEBUG
     ini_set('display_errors', 1);
-    
+
     session_start();
+
+    // 
+    // TEST
+    // 
+    if(!isset($_POST['send'])) { 
+        echo " No valid Post :(";
+    } 
+    else {
+        if (isset($_POST['achternaam'])) {
+            $anaam = $_POST['achternaam'];
+            // do whatever here
+        }
+        
+        if(isset($_POST['voornaam'])) {
+            $vnaam =  $_POST['voornaam'];
+        }
+        // $anaam = mysql_real_escape_string($anaam);
+        // $vnaam = mysql_real_escape_string($vnaam);
+    }
     
+    // 
+    // CONNECTION
+    // 
     $connection = mysqli_connect("localhost","root","1212312121");
     mysqli_select_db($connection, "LearnSomething");
     
