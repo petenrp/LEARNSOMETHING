c660a65338c3a6d46898689a0b0c9087f38cb988
f r e s h <fresh.thit@me.com>
2018-05-27 14:51:02 +0700

    correct name

--- login.php
+++ login.php
@@ -15,7 +15,7 @@
   </head>
   <body>
 
-    <form class="form-signin" action="perform_register.php">
+    <form class="form-signin" action="perform_login.php">
       <div class="mb-4">
         <h1 class="h3 mb-3 font-weight-normal">Login</h1>
       </div>
