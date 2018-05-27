111fa2cc8550a2a0cf15c409d117e9b360bd9b70
pbpeam <32643186+pbpeam@users.noreply.github.com>
2018-05-28 00:15:43 +0700

    ...

---
+++ PurchasedCourse.php
@@ -0,0 +1,41 @@
+<html >
+<head>
+    <link rel="stylesheet" href="CousePage_style.css">
+</head>
+<body>
+    <div class="mainPage">
+    <iframe id="ytplayer" type="text/html" width="100%" height="auto"
+  src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+  frameborder="0"></iframe>
+        <div style="margin: 30px 30px">
+            <h3>Course Title</h3>
+            <div class="indent50">describe: bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~</div>
+            <br>
+            <div class="indent50"><b>by:_______</b></div>
+
+            <p><b>Workshop-Schedule</b></p>
+            (workshop-link)
+
+            <h3>Lession</h3>
+            <div class="h3_font_size, indent30"><b>Lession 1: </b>bla bla bla bla
+            <button class="button" style="float: right">PLAY</button>
+            </div><br>
+            <hr>
+            <br>
+
+            <div class="h3_font_size, indent30"><b>Lession 2: </b>bla bla bla bla
+            <button class="button" style="float: right">PLAY</button>
+            </div><br>
+            <hr>
+            <br>
+
+            <div class="h3_font_size, indent30"><b>Lession 3: </b>bla bla bla bla
+            <button class="button" style="float: right">PLAY</button>
+            </div><br>
+            <hr>
+            <br>
+
+        </div>
+    </body>
+</div>
+</html>
---
+++ notPurchasedCourse.php
@@ -0,0 +1,20 @@
+<html >
+<head>
+    <link rel="stylesheet" href="notPurchasedCourse_style.css">
+</head>
+<body>
+    <div class="mainPage">
+    <iframe id="ytplayer" type="text/html" width="100%" height="auto"
+  src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+  frameborder="0"></iframe>
+        <div style="margin: 30px 30px">
+            <h3>Course Title</h3>
+            <div class="indent50">describe: bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~ bla bla bla~~~</div>
+            <br>
+            <div class="indent50"><b>by:_______</b></div>
+
+            
+        </div>
+    </body>
+</div>
+</html>
