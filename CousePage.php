6d20df49df480b9f6548fab02ff7518426136de6
pbpeam <32643186+pbpeam@users.noreply.github.com>
2018-05-27 20:19:25 +0700

    create coursepage

---
+++ CousePage.php
@@ -0,0 +1,12 @@
+<html >
+<head>
+    <link rel="stylesheet" href="CousePage_style.css">
+</head>
+<body>
+    <div class="mainPage">
+    <iframe id="ytplayer" type="text/html" width="100%" height="auto"
+  src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+  frameborder="0"></iframe>
+    </div>
+</body>
+</html>
