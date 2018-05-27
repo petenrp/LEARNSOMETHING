33686e16eb2a8643d34fb606f7cf942d85f4245c
pbpeam <32643186+pbpeam@users.noreply.github.com>
2018-05-27 21:40:39 +0700

    change loging page color

--- CousePage.php
+++ CousePage.php
@@ -7,6 +7,10 @@
     <iframe id="ytplayer" type="text/html" width="100%" height="auto"
   src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
   frameborder="0"></iframe>
+        <div>
+            <p class="indent">Course Title</p>
+            <p style="overflow:auto, width:90%, height:110px"> description: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</p>
+        </div>
     </div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,9 +1,7 @@
 <?php
-  session_start();
-  if ($_SESSION["email"] != null) {
-    header("location:homepage.html");
+  if ($_SESSION["UserID"] != null) {
+    header("location:main.php");
   }
-  // session_write_close();
 ?>
 
 <html lang="en">
@@ -14,14 +12,14 @@
 
     <!-- CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/login_style.css">
+    <link rel="stylesheet" href="login_style.css">
 
     <!-- FONT -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit|Karla">
 
     <title>Login</title>
   </head>
-  <body>
+  <body style="background-color:#F4F1ED!important ">
 
     <form class="form-signin" name="form1" method="post" action="perform_login.php">
       <div class="mb-4">
@@ -45,17 +43,15 @@
       </div>
       <input class="btn btn-lg btn-primary btn-block" type="submit" name="Submit" value="LOGIN" />
       <div align="right">
-          NOT MEMBER? 
+          NOT MEMBER?
           <!-- <div class="Register-Link"> -->
-            <a href="register.php">REGISTER</a>
+            <a href="register.html">REGISTER</a>
           <!-- </div> -->
       </div>
-
     </form>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-  
   </body>
 </html>
