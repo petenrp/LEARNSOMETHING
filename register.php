1e6c4d638f9f9251b4d551a53b117a929456dbde
pbpeam <32643186+pbpeam@users.noreply.github.com>
2018-05-27 12:20:25 +0700

    asdf

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 <style>
     /* <link rel="stylesheet" href="> */
 </style>
-    <link rel="stylesheet" href="register_style.css">
+    <link rel="stylesheet" type"text/css" href="register_style.css">
 </head>
 <body>
     <p class"sizepx">Register</p>
