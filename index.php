02d5300b68f79d425d5493284377d60fca4050dc
f r e s h <fresh.thit@me.com>
2018-05-28 11:07:14 +0700

    stylize the price labels

--- index.php
+++ index.php
@@ -115,6 +115,10 @@
               echo "<h3 class='font' style='font-weight: bold'>$title</h3>";
               echo "<h5>BY ".strtoupper($instructor_name)."</h5>";
               // echo "<p>$description</p>";
+
+              echo "<p style='background: #ebebeb; color: #424242; width: fit-content; padding: 2 8px; margin-top: 12px; float: right'>
+                    PURCHASED
+                </p>";
               echo "</a>";
               echo "</div>";
             }
@@ -162,7 +166,7 @@
           echo "<img style='width: 100%' src='img/$img'/>";
           echo "<h3 class='font' style='font-weight: bold'>$title</h3>";
           echo "<h5>BY ".strtoupper($instructor_name)."</h5>";
-          echo "<b><h4 style='float: right'>$currency$price</h4></b>";
+          echo "<b><h4 style='background: #ebebeb; color: #424242; width: fit-content; padding: 2 8px; margin-top: 12px; float: right'>$currency$price</h4></b>";
           // echo "<p>$description</p>";
           echo "</div>";
           echo "</a>";
