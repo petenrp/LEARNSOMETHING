73c89222045a7e8c81f9f9b3f99247451ad9e885
f r e s h <fresh.thit@me.com>
2018-05-28 08:25:18 +0700

    navbar inverse

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
   </head>
 
   <body style="background-color:#F4F1ED">
-    <nav class="navbar navbar-fixed-top" style="width: 100%; height:70px; background: #191917">
+    <nav class="navbar navbar-inverse navbar-fixed-top" style="width: 100%; height:70px; background: #191917">
         <div class="container">
             <a class="navbar-brand" href="http://sleepy.hackathon.ilab.sit.kmutt.ac.th" style="margin: 12px 0; height: 100%;">
                 <img src="img/weblogo.png" alt="logo" style="Height:20px;">
