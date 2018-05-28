f590dc88da8339dbc325967fe9b252ec4283d005
f r e s h <fresh.thit@me.com>
2018-05-28 08:17:19 +0700

    add more debugging code

--- perform_purchase.php
+++ perform_purchase.php
@@ -9,16 +9,10 @@
 
 	$user_id = $GET['user_id'];
 	$course_id = $GET['course_id'];
-    
-    // $password = mysqli_real_escape_string($connection, $_POST['inputPassword']);
-    $password = md5($_POST['inputPassword']);
-
-	// if ($first_name == null || $last_name == null || $email == null || $password == null) {
-	// 	// header("location:register.php");
-	// }
-	// else {
-	// 	echo "NOPE NONE OF THAT IS NULL";
-	// }
+	
+	echo $course_id;
+	echo "\n";
+	echo $user_id;
 
 	$connection = mysqli_connect("localhost","root","1212312121");
     mysqli_select_db($connection, "LearnSomething");
@@ -40,7 +34,7 @@
 			
 	// 	header("location:index.php");
 	// }
-	header("location:view_course.php?id=$course_id");
+	// header("location:view_course.php?id=$course_id");
 
 	mysqli_close($connection);
 ?>
