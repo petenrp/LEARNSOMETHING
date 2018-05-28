d1ebce0fc1b0019c486d1d376ac02950694452d8
f r e s h <fresh.thit@me.com>
2018-05-28 07:10:36 +0700

    [index] data in Explore section are now from the DB

--- index.php
+++ index.php
@@ -18,31 +18,13 @@
     // $course_id = $_GET['id'];
     $user_id = $_SESSION['email'];
 
+    // MY COURSEs
     $strSQL = "SELECT * FROM purchase WHERE user_id = '$user_id'";
-    
-    // echo $strSQL;
+    $myCourseQuery = mysqli_query($connection, $strSQL);
 
-    $objQuery = mysqli_query($connection, $strSQL);
-    // $objResult = mysqli_fetch_array($objQuery, MYSQLI_BOTH);
-    
-    // if(!$objResult)
-    // {
-    //     echo "Not purchased";
-    //     header("location:notPurchasedCourse.php?id=$course_id");
-    // }
-    // else
-    // {
-    //     echo "purchased, enjoy :)";
-    //     // $_SESSION["first_name"] = $objResult["first_name"];
-    //     // $_SESSION["last_name"] = $objResult["last_name"];
-    //     // $_SESSION["email"] = $objResult["email"];
-    //     // $_SESSION["password"] = $objResult["password"];
-
-    //     // session_write_close();
-        
-    //     header("location:PurchasedCourse.php?id=$course_id");
-    // }
-    // mysqli_close($connection);
+    // ALL COURSEs
+    $strSQL = "SELECT * FROM courses";
+    $allCourseQuery = mysqli_query($connection, $strSQL);
 ?>
 
 <!DOCTYPE html>
@@ -76,7 +58,7 @@
     <div class="owl-carousel owl-theme">
         <?php
             // echo "code running";
-            while ($row = mysqli_fetch_array($objQuery, MYSQLI_ASSOC)) {
+            while ($row = mysqli_fetch_array($myCourseQuery, MYSQLI_ASSOC)) {
               echo "<div class='item'>";
               $course_id = $row['course_id'];
 
@@ -124,55 +106,48 @@
         <!-- <div class="item">
             <img class="course-item" src="img/1.jpg"/>
             <h4>course 1</h4>
-        </div>
-        <div class="item">
-            <img class="course-item" src="img/2.jpg"/>
-            <h4>course 2</h4>
-        </div>
-        <div class="item">
-            <img class="course-item" src="img/3.jpg"/>
-            <h4>course 3</h4>
-        </div>
-        <div class="item">
-            <img class="course-item" src="img/4.jpg"/>
-            <h4>course 4</h4>
-        </div>
-        <div class="item ">
-            <img class="course-item" src="img/5.jpg"/>
-            <h4>course 5</h4>
-        </div>
-        <div class="item">
-            <img class="course-item" src="img/6.jpg"/>
-            <h4>course 6</h4>
-        </div> 
-        <div class="item">
-            <img class="course-item" src="img/7.jpg"/>
-            <h4>course 7</h4>
-        </div>
-        <div class="item">
-            <img class="course-item" src="img/8.jpg"/>
-            <h4>course 8</h4>
-        </div>
-        <div class="item">
-            <img class="course-item" src="img/9.jpg"/>
-            <h4>course 9</h4>
-        </div>
-        <div class="item">
-            <img class="course-item" src="img/10.jpg"/>
-            <h4>course 10</h4>
-        </div>
-        <div class="item">
-            <img class="course-item" src="img/11.jpg"/>
-            <h4>course 11</h4>
-        </div>
-        <div class="item">
-            <img class="course-item" src="img/12.jpg"/>
-            <h4>course 12</h4>
-        </div>          -->
+        </div>-->
     </div>
 
     <div style="height: 2px; width: auto; margin-left: 5%; margin-top: 25px; margin-right: 5%; background: #191917"></div>
     <p style="font-family: 'Tajawal', sans-serif; font-weight: bold; font-size: 40px; margin: 25px 75px;">Explore</p>
+
+    <?php
+        // echo "code running";
+        while ($row = mysqli_fetch_array($allCourseQuery, MYSQLI_ASSOC)) {
+          echo "<div class='item'>";
+          
+          // GRAB ALL THE PARAMS
+          $title = $courseResult["title"];
+          $description = $courseResult["description"];
+          $instructor_id = $courseResult["instructor_id"];
+          $img = $courseResult["img"];
+          $price = $courseResult["price"];
+
+          //
+          // FETCH INSTRUCTOR
+          //
+          $strSQL = "SELECT * FROM instructors WHERE id = '$instructor_id'";
+          $instructorQuery = mysqli_query($connection, $strSQL);
+          $instructorResult = mysqli_fetch_array($instructorQuery, MYSQLI_BOTH);
+
+          if(!$instructorResult) {
+            // ERROR
+            echo "error fetching instructor\n";
+          }
+          else {
+              $instructor_name = $instructorResult["name"];
+          }
+          
+          echo "<a href='view_course.php?id=$course_id'>";
+          echo "<img style='height: 200px' src='img/$img'/>";
+          echo "<h3 class='font' style='font-weight: bold'>$title</h3>";
+          echo "<h5>BY ".strtoupper($instructor_name)."</h5>";
+          // echo "<p>$description</p>";
+          echo "</a>";
+          echo "</div>";
+        }
+    ?>
 
     <center>
         <img class="course-item-2" src="img/1.jpg"/>
